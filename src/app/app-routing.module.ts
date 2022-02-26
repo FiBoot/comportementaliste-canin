48f80483bfb58a1ac5fdfadc360f1bd06799372a
FiBoot <fiboot89@gmail.com>
2022-02-26 23:47:02 +0100

    routing + pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AProposComponent } from './pages/a-propos/a-propos.component';
+import { CgvComponent } from './pages/cgv/cgv.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { DiplomesComponent } from './pages/diplomes/diplomes.component';
+import { GalerieComponent } from './pages/galerie/galerie.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TarifsComponent } from './pages/tarifs/tarifs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'a-propos', component: AProposComponent },
+	{ path: 'diplomes', component: DiplomesComponent },
+	{ path: 'tarifs', component: TarifsComponent },
+	{ path: 'galerie', component: GalerieComponent },
+	{ path: 'contact', component: ContactComponent },
+	{ path: 'cgv', component: CgvComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AProposComponent } from './pages/a-propos/a-propos.component';
+import { DiplomesComponent } from './pages/diplomes/diplomes.component';
+import { TarifsComponent } from './pages/tarifs/tarifs.component';
+import { GalerieComponent } from './pages/galerie/galerie.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CgvComponent } from './pages/cgv/cgv.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AProposComponent,
+    DiplomesComponent,
+    TarifsComponent,
+    GalerieComponent,
+    ContactComponent,
+    CgvComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
